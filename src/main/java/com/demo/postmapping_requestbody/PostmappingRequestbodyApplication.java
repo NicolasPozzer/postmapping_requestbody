a36fd2c5aec293ddb7776bfda703578ce8aa577e
NicolasPozzer <pozzernico@gmail.com>
2023-09-08 11:00:39 -0300

    publish-in-git

---
+++ src/main/java/com/demo/postmapping_requestbody/PostmappingRequestbodyApplication.java
@@ -0,0 +1,13 @@
+package com.demo.postmapping_requestbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostmappingRequestbodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostmappingRequestbodyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/postmapping_requestbody/PostmappingRequestbodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.postmapping_requestbody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostmappingRequestbodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
